2cd4a9f47825cb716a2cd2a145a88e623caae605
Mahmoud <opssheeh@gmail.com>
2021-12-01 13:54:02 +0800

    WIP: constructing the map graph DS

---
+++ util/graph.js
@@ -0,0 +1,14 @@
+/* 
+  file: graph.js
+  author: mahmoud
+
+  represent the map used by the car as a graph structure to aid 
+  finding the shortest path, for now we always start from the same node 
+  and find the shortest path to any node in the map
+*/
+
+class Graph {
+  contructor() {
+
+  }
+}
